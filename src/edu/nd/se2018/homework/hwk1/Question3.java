b6b897afff8f579dd479534de291efe603334840
Patrick Falvey <patrickjafalvey@gmail.com>
2018-09-02 18:21:23 -0400

    finished hw

--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -4,6 +4,21 @@ public class Question3 {
 	public Question3(){}	
 	
     public int getMirrorCount(int[] numbers){
-		return 0;	
+    	int count = 0;
+    	boolean isMirror = false;
+    	int length = numbers.length;
+    	//loop through array checking for similar digits
+    	for (int i = 0; i < length; i++) {
+    		if (numbers[i] == numbers[length - 1 - i]) { //minus one to account for indexing
+    			if (count == 0 || isMirror) {
+    				count += 1;
+    				isMirror = true;
+    			}
+    		}
+    		else {
+    			isMirror = false;
+    		}
+    	}
+		return count;	
 	}
 }
