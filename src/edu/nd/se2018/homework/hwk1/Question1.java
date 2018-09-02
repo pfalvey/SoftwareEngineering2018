ed8fe978ce83fd684dd62f63aeb44b328fe29b04
Patrick Falvey <patrickjafalvey@gmail.com>
2018-09-02 17:35:17 -0400

    finish q1

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,20 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashSet;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		HashSet<Integer> h = new HashSet<Integer>();
+		for (int x : numbers) {
+			h.add(x);
+		}
+		int sum = 0;
+		for (Integer x: h) {
+			sum += x;
+		}
+		return sum;	
 	}
 }
