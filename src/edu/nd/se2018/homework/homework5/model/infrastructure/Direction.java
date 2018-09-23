c4eab7d502e3f37f3d0492d15d8aa38000fa53d2
Patrick Falvey <patrickjafalvey@gmail.com>
2018-09-23 16:34:18 -0400

    Update and refactor code for homework 5 folder

---
+++ src/edu/nd/se2018/homework/homework5/model/infrastructure/Direction.java
@@ -0,0 +1,9 @@
+package edu.nd.se2018.homework.homework5.model.infrastructure;
+
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    CUSTOM;
+}
---
+++ src/edu/nd/se2018/homework/homework5/view/IDisplay.java
@@ -0,0 +1,5 @@
+package edu.nd.se2018.homework.homework5.view;
+
+public interface IDisplay {
+	public void draw();
+}
